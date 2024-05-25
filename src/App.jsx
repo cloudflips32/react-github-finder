61cf424008ff6748fe2ff1e3a3aa561a52181bd3
Adam Standish <devadamstandish@gmail.com>
2024-05-25 16:58:08 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <button className="btn">Click</button>
+      </div>
+    </>
+  )
+}
+
+export default App
