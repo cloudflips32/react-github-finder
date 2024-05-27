2483fe81edb6c87ba4c615ec7ce53ef51a35d61e
Adam Standish <devadamstandish@gmail.com>
2024-05-27 19:12:30 -0400

    footer, svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
 
         <main>Content</main>
+        <Footer />
       </div>
     </Router>
   )
